44ee9127008b2dd0da5bb7b4831d7c8b338fe3b2
Irmen de Jong <irmen@razorvine.net>
2019-01-15 17:15:28 +0100

    fixed cmd scripts

--- compile.cmd
+++ compile.cmd
@@ -1,8 +1,5 @@
-set PROG8_COMPILER_DIR=compiler
-
-set PROG8_LIBDIR=%PROG8_COMPILER_DIR%/prog8lib
-set PROG8CLASSPATH=%PROG8_COMPILER_DIR%/out/production/compiler/
+set PROG8CLASSPATH=./out/production/compiler;./out/production/parser
 set KOTLINPATH=%USERPROFILE%/.IdeaIC2018.3/config/plugins/Kotlin
-set LIBJARS=%KOTLINPATH%/lib/kotlin-stdlib.jar;%KOTLINPATH%/lib/kotlin-reflect.jar;%PROG8_COMPILER_DIR%/antlr/lib/antlr-runtime-4.7.2.jar
+set LIBJARS=%KOTLINPATH%/lib/kotlin-stdlib.jar;%KOTLINPATH%/lib/kotlin-reflect.jar;./parser/antlr/lib/antlr-runtime-4.7.2.jar
 
-java -Dprog8.libdir=%PROG8_LIBDIR% -cp %PROG8CLASSPATH%;%LIBJARS% prog8.CompilerMainKt %*
+java -cp %PROG8CLASSPATH%;%LIBJARS% prog8.CompilerMainKt %*
