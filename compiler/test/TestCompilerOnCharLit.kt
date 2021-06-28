006059438ff7b89b8b73c7f4d096c3ee24925744
meisl <kling@sprax.org>
2021-06-28 18:49:01 +0200

    + same warning on the other "TestCompilerXyz" file

--- compiler/test/TestCompilerOnCharLit.kt
+++ compiler/test/TestCompilerOnCharLit.kt
@@ -15,6 +15,14 @@ import prog8.compiler.target.Cx16Target
 import prog8.compiler.compileProgram
 
 
+/**
+ * ATTENTION: this is just kludge!
+ * They are not really unit tests, but rather tests of the whole process,
+ * from source file loading all the way through to running 64tass.
+ * What's more: in case of failure (to compile and assemble) - which is when tests should help you -
+ * these tests will actually be ignored (ie. NOT fail),
+ * because in the code there are calls to Process.exit, making it essentially untestable.
+ */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TestCompilerOnCharLit {
     val workingDir = Path("").absolute()    // Note: Path(".") does NOT work..!
