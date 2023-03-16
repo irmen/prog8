b399b0f18290caf81f61bff05ab710c6e133f7cc
Irmen de Jong <irmen@razorvine.net>
2023-03-16 23:37:33 +0100

    don't print weird position link for dummy positions

--- codeCore/src/prog8/code/core/Position.kt
+++ codeCore/src/prog8/code/core/Position.kt
@@ -7,6 +7,9 @@ import kotlin.io.path.absolute
 data class Position(val file: String, val line: Int, val startCol: Int, val endCol: Int) {
     override fun toString(): String = "[$file: line $line col ${startCol+1}-${endCol+1}]"
     fun toClickableStr(): String {
+        if(this===DUMMY) {
+            return ""
+        }
         return try {
             val path = Path(file).absolute().normalize().toString()
             "file://$path:$line:$startCol:"
