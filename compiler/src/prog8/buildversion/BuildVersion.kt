13ab4166c01ab4ad06b1ef9647c5bb6ad1a57701
Irmen de Jong <irmen@razorvine.net>
2023-10-19 21:57:06 +0200

    new kotest library version

--- compiler/src/prog8/buildversion/BuildVersion.kt
+++ compiler/src/prog8/buildversion/BuildVersion.kt
@@ -5,11 +5,11 @@ package prog8.buildversion
  */
 const val MAVEN_GROUP = "prog8"
 const val MAVEN_NAME = "compiler"
-const val VERSION = "9.5"
-const val GIT_REVISION = 4149
-const val GIT_SHA = "203ec5fa46f883885b7e66526f18770aa1717785"
-const val GIT_DATE = "2023-10-15T18:24:48Z"
+const val VERSION = "9.5.1"
+const val GIT_REVISION = 4157
+const val GIT_SHA = "3dc5a0e7f8e5a94fdb4c13ff9199a4bf912268f2"
+const val GIT_DATE = "2023-10-18T21:59:37Z"
 const val GIT_BRANCH = "master"
-const val BUILD_DATE = "2023-10-15T19:52:30Z"
-const val BUILD_UNIX_TIME = 1697399550365L
+const val BUILD_DATE = "2023-10-19T19:55:55Z"
+const val BUILD_UNIX_TIME = 1697745355291L
 const val DIRTY = 1
