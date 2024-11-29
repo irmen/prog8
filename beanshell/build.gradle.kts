da03941582dd739c438695c75017899aa4404b13
Irmen de Jong <irmen@razorvine.net>
2024-11-29 01:01:59 +0100

    fix build

--- beanshell/build.gradle.kts
+++ beanshell/build.gradle.kts
@@ -5,9 +5,17 @@ plugins {
     kotlin("jvm")
 }
 
+java {
+    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin {
+    compilerOptions.jvmTarget = JvmTarget.JVM_11
+}
+
 val serverMainClassName = "prog8lsp.MainKt"
 val applicationName = "prog8-beanshell"
-val javaVersion: String by project
 
 application {
     mainClass.set(serverMainClassName)
@@ -45,11 +53,6 @@ sourceSets.main {
     resources.srcDir("resources")
 }
 
-sourceSets.test {
-    java.srcDir("src")
-    resources.srcDir("resources")
-}
-
 tasks.startScripts {
     applicationName = "prog8-beanshell"
 }
@@ -62,26 +65,10 @@ tasks.register<Exec>("fixFilePermissions") {
     commandLine("chmod", "+x", "${tasks.installDist.get().destinationDir}/bin/prog8-beanshell")
 }
 
-tasks.withType<Test>() {
-    testLogging {
-        events("failed")
-        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-    }
-}
-
 tasks.installDist {
     finalizedBy("fixFilePermissions")
 }
 
 tasks.build {
     finalizedBy("installDist")
-}
-
-java {
-    targetCompatibility = JavaVersion.VERSION_11
-    sourceCompatibility = JavaVersion.VERSION_11
-}
-
-kotlin {
-    compilerOptions.jvmTarget = JvmTarget.JVM_11
 }
