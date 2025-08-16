976bd529726f1ec86dd2fee2a6a3b8953fcee458
Irmen de Jong <irmen@razorvine.net>
2025-08-16 11:32:24 +0200

    docs dark mode

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,10 @@ needs_sphinx = '5.3'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ 'sphinxcontrib.jquery']
+extensions = [ 'sphinxcontrib.jquery', 'sphinx_rtd_dark_mode']
+
+# user starts in light mode
+default_dark_mode = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
